47b969c553f964784663e5027a2f7223ffc6674f
Megha494 <101010392+Megha494@users.noreply.github.com>
2022-04-19 01:09:47 +0530

    Add files via upload

---
+++ vars/jenkinsTomcat.groovy
@@ -0,0 +1,45 @@
+def call(String registryin = 'a', String docTag = 'a', String grepo = 'a', String gbranch = 'a') {
+pipeline {
+environment { 
+    registry = "$registryin"   
+    dockerTag = "${docTag}$BUILD_NUMBER"
+    gitRepo = "${grepo}"
+    gitBranch = "${gbranch}"
+  }
+  
+  agent none
+  
+  stages {
+	stage('check out') {
+		agent {label 'docker'}
+		steps {
+			checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/Megha494/tomcat-config.git']]])
+		}
+	}
+
+	stage('BUILD IMAGE') {
+		agent {label 'docker'}
+		steps {
+			script {
+				dockerImage=docker.build("'$registry:$dockerTag'")
+			}
+		}
+	}
+
+	stage('PUSH TO DOCKER HUB') {
+		agent {label 'docker'}
+		steps {
+			dockerImage.push()
+		
+		}
+	}
+
+	stage('DEPLOY') {
+		agent {label 'kubernetes'}
+		steps {
+			sh 'kubectl set image deployment webapp-deployment tomcat="$registry:$dockerTag" --record'
+		}
+	}
+}
+
+  
